2385df18820c48ff03628662cf9e126268fcab93
Roman Samoilov <2270393+rsamoilov@users.noreply.github.com>
2023-09-21 12:55:07 +0100

    Add file tests

--- spec/fiber_scheduler_spec.rb
+++ spec/fiber_scheduler_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "net/http"
+require "digest"
 
 RSpec.describe Rage::FiberScheduler do
   TEST_HTTP_URL = ENV.fetch("TEST_HTTP_URL")
@@ -16,5 +17,48 @@ RSpec.describe Rage::FiberScheduler do
       result = Net::HTTP.get(URI("#{TEST_HTTP_URL}/long-http-get?i=#{num}"))
       -> { expect(result.to_f).to eq(num * 10) }
     end
+  end
+
+  it "correctly reads files" do
+    within_reactor do
+      str = File.read("spec/fixtures/700b.txt")
+      -> { expect(Digest::SHA2.hexdigest(str)).to eq("174d893e34d9e551466f9e6808358cebc7fd91c5cf9a07300a18e035abc0c4a9") }
+    end
+  end
+
+  it "correctly reads files" do
+    within_reactor do
+      str = File.read("spec/fixtures/2kb.txt")
+      -> { expect(Digest::SHA2.hexdigest(str)).to eq("73ad9fa79f6266fad72c925e5ba197ba296aa433ae5d0f792e87c19e79df798a") }
+    end
+  end
+
+  it "correctly reads files" do
+    within_reactor do
+      str = File.read("spec/fixtures/10kb.txt")
+      -> { expect(Digest::SHA2.hexdigest(str)).to eq("ec6f95fa1b9b256aeed3d21c0b982822e642079dabd5a032929a993b614815d8") }
+    end
+  end
+
+  it "correctly writes files" do
+    within_reactor do
+      str = 100.times.map { rand(1..100) }.join
+      File.write("test", str)
+
+      -> { expect(File.read("test")).to eq(str) }
+    end
+
+    File.unlink("test")
+  end
+
+  it "correctly writes files" do
+    within_reactor do
+      str = 500_000.times.map { rand(1..100) }.join
+      File.write("test", str)
+
+      -> { expect(File.read("test")).to eq(str) }
+    end
+
+    File.unlink("test")
   end
 end
