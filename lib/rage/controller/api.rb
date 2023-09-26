b4aef97d6fc39d9b87b0cf832b8f4b7813cc78ff
Roman Samoilov <2270393+rsamoilov@users.noreply.github.com>
2023-09-26 13:30:33 +0100

    dummy change

--- lib/rage/controller/api.rb
+++ lib/rage/controller/api.rb
@@ -83,7 +83,7 @@ class RageController::API
     #   end
     # @example
     #   rescue_from User::NotAuthorized do |_|
-    #     head :forbidden
+    #     head 400
     #   end
     # @note Unlike Rails, the handler must always take an argument. Use `_` if you don't care about the actual exception.
     def rescue_from(*klasses, with: nil, &block)
