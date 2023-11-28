be5d10f170cb0d55e3d85518e913bcb2a23ab43c
Oleksandr Rohachev <oleksandr.rohachev@wework.com>
2023-11-28 16:47:23 +0200

    check

--- lib/rage/setup.rb
+++ lib/rage/setup.rb
@@ -2,22 +2,4 @@ Iodine.patch_rack
 
 require_relative "#{Rage.root}/config/environments/#{Rage.env}"
 
-
-# load application files
-app, bad = Dir["#{Rage.root}/app/**/*.rb"], []
-
-loop do
-  path = app.shift
-  break if path.nil?
-
-  require_relative path
-
-# push the file to the end of the list in case it depends on another file that has not yet been required;
-# re-raise if only errored out files are left
-rescue NameError
-  raise if (app - bad).empty?
-  app << path
-  bad << path
-end
-
 require_relative "#{Rage.root}/config/routes"
