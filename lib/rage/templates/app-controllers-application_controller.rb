49a40fafb829967355b9a131c39a618152faca3a
Roman Samoilov <2270393+rsamoilov@users.noreply.github.com>
2023-11-29 19:01:30 +0000

    Log params in development and test

--- lib/rage/templates/app-controllers-application_controller.rb
+++ lib/rage/templates/app-controllers-application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < RageController::API
+  if Rage.env.development? || Rage.env.test?
+    before_action do
+      Rage.logger.with_context(params: params) { Rage.logger.debug("parameters") }
+    end
+  end
 end
