9a97786d76a9e47b3a81ffef0d085a0094e7ffc8
Roman Samoilov <2270393+rsamoilov@users.noreply.github.com>
2023-12-11 16:44:08 +0000

    Schedule fibers in a middleware
    this allows to be 100% compatible with custom Rack middlewares

--- lib/rage/configuration.rb
+++ lib/rage/configuration.rb
@@ -8,6 +8,10 @@ class Rage::Configuration
     @server ||= Server.new
   end
 
+  def middleware
+    @middleware ||= Middleware.new
+  end
+
   class Server
     attr_accessor :port, :workers_count
     attr_reader :threads_count
@@ -16,6 +20,43 @@ class Rage::Configuration
       @threads_count = 1
       @workers_count = -1
       @port = 3000
+    end
+  end
+
+  class Middleware
+    attr_reader :middlewares
+
+    def initialize
+      @middlewares = [[Rage::FiberWrapper]]
+    end
+
+    def use(new_middleware, *args, &block)
+      insert_after(@middlewares.length - 1, new_middleware, *args, &block)
+    end
+
+    def insert_before(existing_middleware, new_middleware, *args, &block)
+      index = find_middleware_index(existing_middleware)
+      @middlewares = (@middlewares[0...index] + [[new_middleware, args, block]] + @middlewares[index..]).uniq(&:first)
+    end
+
+    def insert_after(existing_middleware, new_middleware, *args, &block)
+      index = find_middleware_index(existing_middleware)
+      @middlewares = (@middlewares[0..index] + [[new_middleware, args, block]] + @middlewares[index + 1..]).uniq(&:first)
+    end
+
+    private
+
+    def find_middleware_index(middleware)
+      if middleware.is_a?(Integer)
+        if middleware < 0 || middleware >= @middlewares.length
+          raise ArgumentError, "Middleware index should be in the (0...#{@middlewares.length}) range"
+        end
+        middleware
+      else
+        @middlewares.index { |m, _, _| m == middleware }.tap do |i|
+          raise ArgumentError, "Couldn't find #{middleware} in the middleware stack" unless i
+        end
+      end
     end
   end
 
