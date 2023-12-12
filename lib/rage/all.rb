13994a491ad083633b993a188556f2084d0d0b9d
Roman Samoilov <2270393+rsamoilov@users.noreply.github.com>
2023-12-12 18:56:36 +0000

    Add CORS middleware

--- lib/rage/all.rb
+++ lib/rage/all.rb
@@ -23,6 +23,7 @@ require_relative "logger/text_formatter"
 require_relative "logger/logger"
 
 require_relative "middleware/fiber_wrapper"
+require_relative "middleware/cors"
 
 if defined?(Sidekiq)
   require_relative "sidekiq_session"
