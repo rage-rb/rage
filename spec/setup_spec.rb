569b9b6eb5286a8f5ec0dff3662afefb2aae2c1c
Roman Samoilov <2270393+rsamoilov@users.noreply.github.com>
2024-09-13 17:04:42 +0100

    Fix tests

--- spec/setup_spec.rb
+++ spec/setup_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Setup" do
 
   before do
     allow(Rage).to receive(:env).and_return(env)
-    allow(Rage).to receive(:root).and_return(File.expand_path("..", __dir__))
+    allow(Rage).to receive(:root).and_return(Pathname.new(File.expand_path("..", __dir__)))
     allow(Rage).to receive_message_chain(:code_loader, :setup).and_return(true)
     allow(Iodine).to receive(:patch_rack).and_return(true)
   end
