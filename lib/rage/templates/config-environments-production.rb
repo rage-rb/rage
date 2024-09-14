9bca41ffed1191b888b52f0b170513b774275049
Roman Samoilov <2270393+rsamoilov@users.noreply.github.com>
2024-09-14 17:13:56 +0100

    Log to STDOUT by default in production

--- lib/rage/templates/config-environments-production.rb
+++ lib/rage/templates/config-environments-production.rb
@@ -1,11 +1,11 @@
 Rage.configure do
   # Specify the number of server processes to run. Defaults to number of CPU cores.
-  # config.server.workers_count = ENV.fetch("WEB_CONCURRENCY", 1)
+  # config.server.workers_count = ENV.fetch("WEB_CONCURRENCY", 1).to_i
 
   # Specify the port the server will listen on.
   config.server.port = 3000
 
   # Specify the logger
-  config.logger = Rage::Logger.new("log/production.log")
+  config.logger = Rage::Logger.new(STDOUT)
   config.log_level = Logger::INFO
 end
