c5b1b66581ce6d4b2a4a3ddc28cacfa18026af3f
Roman Samoilov <2270393+rsamoilov@users.noreply.github.com>
2024-12-17 19:27:30 +0000

    Fix integration test helper

--- spec/support/integration_helper.rb
+++ spec/support/integration_helper.rb
@@ -3,7 +3,8 @@
 module IntegrationHelper
   def launch_server(env: {})
     Bundler.with_unbundled_env do
-      system("gem build -o rage-local.gem && gem install rage-local.gem --no-document && bundle install")
+      system("gem build -o rage-local.gem && gem install rage-local.gem --no-document")
+      system("bundle install", chdir: "spec/integration/test_app")
       @pid = spawn(env, "bundle exec rage s", chdir: "spec/integration/test_app")
       sleep(1)
     end
