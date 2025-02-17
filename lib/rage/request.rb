619424148097a73646d4bc6a3b8f55bbb5f7955f
Steven Fernandez <stevenfernandez@Stevens-MacBook-Air.local>
2025-02-17 13:35:41 -0400

    Add yard changes

--- lib/rage/request.rb
+++ lib/rage/request.rb
@@ -3,6 +3,7 @@
 require "time"
 
 class Rage::Request
+  # regexp to match against a ip address
   IP_HOST_REGEXP  = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/
   # HTTP methods from [RFC 2616: Hypertext Transfer Protocol -- HTTP/1.1](https://www.ietf.org/rfc/rfc2616.txt)
   RFC2616 = %w(OPTIONS GET HEAD POST PUT DELETE TRACE CONNECT)
@@ -21,13 +22,13 @@ class Rage::Request
   # HTTP methods from [RFC 5789: PATCH Method for HTTP](https://www.ietf.org/rfc/rfc5789.txt)
   RFC5789 = %w(PATCH)
 
+  # Set data structure of all RFC defined HTTP headers
   HTTP_METHODS_SET = (RFC2616 + RFC2518 + RFC3253 + RFC3648 + RFC3744 + RFC5323 + RFC4791 + RFC5789).to_set
 
   attr_accessor :env
   # @private
   def initialize(env, custom_proxies: nil)
     @env = env
-    # super(env)
     after_initialize(custom_proxies) if custom_proxies
   end
 
@@ -41,78 +42,119 @@ class Rage::Request
     end
   end
 
-  def rack_request
-    @rack_request ||= Rack::Request.new(@env)
-  end
-
+  # Checks if the request was made using TLS/SSL which is if http or https protocol is used inside the URL.
+  # @return [Boolean] true if the request is TLS/SSL, false otherwise
   def ssl?
     rack_request.ssl?
   end
 
+  # Gets the hostname from the request
+  # @return [String] the hostname
   def host
     rack_request.host
   end
 
+  # Gets the port number from the request
+  # @return [Integer] the port number
   def port
     rack_request.port
   end
 
+  # Gets the query string from the request
+  # @return [String] the query string (empty string if no query)
   def query_string
     rack_request.query_string
   end
 
+  # Gets the environment hash
+  # @return [Hash] the environment variables hash
   def env
     @env
   end
 
+  # Gets the specified HTTP header value
+  # @param name [String] the name of the header to retrieve
+  # @return [String, nil] the header value or nil if not found
   def get_header(name)
     rack_request.get_header(name)
   end
 
+  # Checks if the request uses GET method
+  # @return [Boolean] true if GET request
   def get?
     rack_request.get?
   end
 
+  # Checks if the request uses POST method
+  # @return [Boolean] true if POST request
   def post?
     rack_request.post?
   end
 
+  # Checks if the request uses PATCH method
+  # @return [Boolean] true if PATCH request
   def patch?
     rack_request.patch?
   end
 
+  # Checks if the request uses PUT method
+  # @return [Boolean] true if PUT request
   def put?
     rack_request.put?
   end
 
+  # Checks if the request uses DELETE method
+  # @return [Boolean] true if DELETE request
   def delete?
     rack_request.delete?
   end
 
+  # Checks if the request uses HEAD method
+  # @return [Boolean] true if HEAD request
   def head?
     rack_request.head?
   end
 
+  # Gets the full request URL
+  # @return [String] complete URL including protocol, host, path, and query
   def url
     rack_request.url
   end
 
+  # Gets the request path
+  # @return [String] the path component of the URL
+  # @example
+  #   request.path # => "/users"
   def path
     rack_request.path
   end
 
+  # Gets the full path including query string
+  # @return [String] path with query string (if present)
+  # @example
+  #   request.fullpath # => "/users?show_archived=true"
   def fullpath
     rack_request.fullpath
   end
 
+  # Gets the User-Agent header value
+  # @return [String, nil] the user agent string or nil
+  # @example
+  #  request.user_agent # => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
   def user_agent
     rack_request.user_agent
   end
 
+  # Gets the content type of the request
+  # @return [String, nil] the MIME type of the request body
   def format
     rack_request.content_type
   end
 
+  # Gets the client IP address. There is a special consideration here with proxies
+  # or when (`X-Forwarded-For` or `Forwarded`) headers are being used.
+  # See [Rack::Request.ip](https://rubydoc.info/gems/rack/Rack%2FRequest%2FHelpers:ip)
+  # @return [String] the remote IP address
   def remote_ip
     rack_request.ip
   end
@@ -146,48 +188,28 @@ class Rage::Request
     )
   end
 
-  # Returns the path of the request.
-  # @example
-  #   request.path # => "/users"
-  # def path
-  #   @env["PATH_INFO"]
-  # end
-
-  # Returns the full path including the query string.
-  # @example
-  #   request.fullpath # => "/users?show_archived=true"
-  # def fullpath
-  #   path = @env["PATH_INFO"]
-  #   query_string = @env["QUERY_STRING"]
-  #   query_string.empty? ? path : "#{path}?#{query_string}"
-  # end
-
-  # Returns the user agent of the request.
-  # @example
-  #  request.user_agent # => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
-  # def user_agent
-  #   @env["HTTP_USER_AGENT"]
-  # end
-
+  # Returns the domain of the request.
+  # @param tld_length [Integer] specify tld_length levels down into domain tree
+  # @example Consider a URL like: "example.foo.gov"
+  #   request.domain => "foo.gov"
+  #   request.domain(0) => "gov"
+  #   request.domain(2) => "example.foo.gov"
   def domain(tld_length = 1)
     extract_domain(host, tld_length)
   end
 
-  def protocol
-    (ssl? == "https://") ? "https://" : "http://"
-  end
-
-  def method(*args)
-    if args.empty?
-      @method ||= check_method(
-        get_header("rack.methodoverride.original_method") || get_header("REQUEST_METHOD")
-      )
-    else
-      super
-    end
+  # Gets the HTTP method of the request. If the client is using `Rack::MethodOverride`
+  # middleware then the `X-HTTP-Method-Override` header is checked before `REQUEST_METHOD`
+  # @return [String] The HTTP Method override header or the request method header
+  def method
+    check_method(get_header("rack.methodoverride.original_method") || get_header("REQUEST_METHOD"))
   end
 
   private
+
+  def rack_request
+    @rack_request ||= Rack::Request.new(@env)
+  end
 
   def check_method(name)
     http_methods_set = HTTP_METHODS_SET
