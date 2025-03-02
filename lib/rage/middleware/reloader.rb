0c055023d176151c9bb4971cae09aefc1e692271
Roman Samoilov <2270393+rsamoilov@users.noreply.github.com>
2025-03-02 13:44:22 +0000

    Reload code in development based on filesystem updates

--- lib/rage/middleware/reloader.rb
+++ lib/rage/middleware/reloader.rb
@@ -6,11 +6,25 @@ class Rage::Reloader
   end
 
   def call(env)
-    Rage.code_loader.reload
-    @app.call(env)
+    with_reload do
+      @app.call(env)
+    end
   rescue Exception => e
     exception_str = "#{e.class} (#{e.message}):\n#{e.backtrace.join("\n")}"
     puts(exception_str)
     [500, {}, [exception_str]]
+  end
+
+  private
+
+  def with_reload
+    if Rage.code_loader.check_updated!
+      Fiber.new(blocking: true) {
+        Rage.code_loader.reload
+        yield
+      }.resume
+    else
+      yield
+    end
   end
 end
