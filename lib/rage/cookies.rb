10c20224bfc80e6338c4140b8cd6495828ffb51f
Roman Samoilov <2270393+rsamoilov@users.noreply.github.com>
2026-01-26 18:17:12 +0000

    Update error messages

--- lib/rage/cookies.rb
+++ lib/rage/cookies.rb
@@ -6,7 +6,7 @@ require "time"
 if !defined?(DomainName)
   fail <<~ERR
 
-    rage-rb depends on domain_name to specify the domain name for cookies. Add the following line to your Gemfile:
+    Rage depends on `domain_name` to specify the domain name for cookies. Ensure the following line is added to your Gemfile:
     gem "domain_name"
 
   ERR
@@ -266,7 +266,7 @@ class Rage::Cookies
           Rage.logger.debug("Failed to decrypt encrypted cookie")
           i ||= 0
           if (box = fallback_boxes[i])
-            Rage.logger.debug("Trying to decrypt with fallback key ##{i + 1}")
+            Rage.logger.debug { "Trying to decrypt with fallback key ##{i + 1}" }
             i += 1
             retry
           end
@@ -286,7 +286,7 @@ class Rage::Cookies
           if !defined?(RbNaCl) || !(Gem::Version.create(RbNaCl::VERSION) >= Gem::Version.create("3.3.0") && Gem::Version.create(RbNaCl::VERSION) < Gem::Version.create("8.0.0"))
             fail <<~ERR
 
-              rage-rb depends on rbnacl [>= 3.3, < 8.0] to encrypt cookies. Add the following line to your Gemfile:
+              Rage depends on `rbnacl` [>= 3.3, < 8.0] to encrypt cookies. Ensure the following line is added to your Gemfile:
               gem "rbnacl"
 
             ERR
