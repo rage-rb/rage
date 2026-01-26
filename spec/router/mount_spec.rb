953224ed484d946c3d35207fbd7424df627d40ea
Roman Samoilov <2270393+rsamoilov@users.noreply.github.com>
2026-01-26 18:16:43 +0000

    Rely on Rack::Session for mounted apps

--- spec/router/mount_spec.rb
+++ spec/router/mount_spec.rb
@@ -46,4 +46,60 @@ RSpec.describe Rage::Router::Backend do
       router.mount("/rack_app", 5, %w(GET))
     }.to raise_error(/should respond to `call`/)
   end
+
+  context "with session" do
+    let(:app) do
+      Class.new do
+        def self.name
+          "Sidekiq::Web"
+        end
+
+        def self.call(env)
+        end
+      end
+    end
+
+    context "with Rack::Session available" do
+      let(:session) do
+        Class.new do
+          def initialize(app, **)
+            @app = app
+          end
+
+          def call(env)
+            :test_session
+          end
+        end
+      end
+
+      before do
+        allow(Rage).to receive(:config).and_return(double(secret_key_base: "test secret"))
+        stub_const("Rack::Session::Cookie", session)
+      end
+
+      it "exposes session object" do
+        router.mount("/test", app, %w(GET))
+
+        response, _ = perform_get_request("/test")
+        expect(response).to eq(:test_session)
+      end
+
+      it "rewinds request body" do
+        router.mount("/test", app, %w(GET))
+
+        body = StringIO.new("test body").tap(&:read)
+        perform_get_request("/test", body:)
+
+        expect(body.read).to eq("test body")
+      end
+    end
+
+    context "with Rack::Session unavailable" do
+      it "raises exception" do
+        expect {
+          router.mount("/test", app, %w(GET))
+        }.to raise_error(/`Sidekiq::Web` depends on `Rack::Session`/)
+      end
+    end
+  end
 end
