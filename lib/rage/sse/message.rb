f1c8d1edcbe5c30f4de2a546c3ebf1117b43c295
Roman Samoilov <2270393+rsamoilov@users.noreply.github.com>
2026-02-26 17:28:14 +0000

    Update message generation logic

--- lib/rage/sse/message.rb
+++ lib/rage/sse/message.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # A class representing an SSE event. Use it to specify the `id`, `event`, and `retry` fields in an SSE.
 #
 # @!attribute id
@@ -10,13 +12,14 @@
 #   @return [String, #to_json] The `data` field of the SSE event. If it's an object, it will be serialized to JSON.
 Rage::SSE::Message = Struct.new(:id, :event, :retry, :data, keyword_init: true) do
   def to_s
-    str = ""
+    data_entry = if !data.is_a?(String)
+      "data: #{data.to_json}\n"
+    elsif data.include?("\n")
+      data.split("\n").map { |d| "data: #{d}\n" }.join
+    else
+      "data: #{data}\n"
+    end
 
-    str << "id: #{id}\n" if id
-    str << "event: #{event}\n" if event
-    str << "retry: #{self.retry}\n" if self.retry
-    str << "data: #{data.is_a?(String) ? data : data.to_json}\n" if data # TODO: multiline
-
-    str + "\n"
+    "#{data_entry}#{"id: #{id}\n" if id}#{"event: #{event}\n" if event}#{"retry: #{self.retry}\n" if self.retry}\n"
   end
 end
