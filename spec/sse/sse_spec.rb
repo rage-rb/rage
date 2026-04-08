bc5280eaf20c03aceb0def7a7dbe569984ab8432
Roman Samoilov <2270393+rsamoilov@users.noreply.github.com>
2026-04-08 18:03:52 +0100

    Add tests

--- spec/sse/sse_spec.rb
+++ spec/sse/sse_spec.rb
@@ -86,4 +86,167 @@ RSpec.describe Rage::SSE do
       end
     end
   end
+
+  describe ".close_stream" do
+    before do
+      Rage::SSE.__adapter = mock_adapter
+    end
+
+    after do
+      Rage::SSE.__adapter = nil
+    end
+
+    let(:mock_adapter) { double }
+    let(:streamable) { [:test_stream, 123] }
+
+    context "inside the runtime" do
+      before do
+        allow(Iodine).to receive(:running?).and_return(true)
+      end
+
+      it "broadcasts the close message" do
+        allow(Rage::Internal).to receive(:stream_name_for).with(streamable).and_return("test-stream-123")
+
+        expect(Rage::SSE::InternalBroadcast).to receive(:broadcast).with("test-stream-123", Rage::SSE::CLOSE_STREAM_MSG, Iodine::PubSub::CLUSTER)
+        expect(mock_adapter).to receive(:publish).with(Rage::SSE::PUBSUB_BROADCASTER_ID, "test-stream-123", Rage::SSE::CLOSE_STREAM_MSG)
+
+        described_class.close_stream(streamable)
+      end
+    end
+
+    context "outside the runtime" do
+      before do
+        allow(Iodine).to receive(:running?).and_return(false)
+      end
+
+      it "broadcasts the close message" do
+        allow(Rage::Internal).to receive(:stream_name_for).with(streamable).and_return("test-stream-123")
+
+        expect(Rage::SSE::InternalBroadcast).not_to receive(:broadcast)
+        expect(mock_adapter).to receive(:publish).with(Rage::SSE::PUBSUB_BROADCASTER_ID, "test-stream-123", Rage::SSE::CLOSE_STREAM_MSG)
+
+        described_class.close_stream(streamable)
+      end
+    end
+
+    context "without the adapter" do
+      before do
+        allow(Iodine).to receive(:running?).and_return(true)
+      end
+
+      let(:mock_adapter) { nil }
+
+      it "broadcasts the close message" do
+        allow(Rage::Internal).to receive(:stream_name_for).with(streamable).and_return("test-stream-123")
+        expect(Rage::SSE::InternalBroadcast).to receive(:broadcast).with("test-stream-123", Rage::SSE::CLOSE_STREAM_MSG, Iodine::PubSub::CLUSTER)
+
+        expect { described_class.close_stream(streamable) }.not_to raise_error
+      end
+    end
+  end
+
+  describe ".broadcast" do
+    before do
+      Rage::SSE.__adapter = mock_adapter
+    end
+
+    after do
+      Rage::SSE.__adapter = nil
+    end
+
+    let(:mock_adapter) { double }
+    let(:streamable) { [:test_stream, 123] }
+
+    let(:message) { "message" }
+    let(:serialized_message) { "serialized-message" }
+
+    context "inside the runtime" do
+      before do
+        allow(Iodine).to receive(:running?).and_return(true)
+      end
+
+      it "broadcasts the message" do
+        allow(Rage::Internal).to receive(:stream_name_for).with(streamable).and_return("test-stream-123")
+        allow(Rage::SSE).to receive(:__serialize).with(message).and_return(serialized_message)
+
+        expect(Rage::SSE::InternalBroadcast).to receive(:broadcast).with("test-stream-123", serialized_message, Iodine::PubSub::CLUSTER)
+        expect(mock_adapter).to receive(:publish).with(Rage::SSE::PUBSUB_BROADCASTER_ID, "test-stream-123", serialized_message)
+
+        described_class.broadcast(streamable, message)
+      end
+    end
+
+    context "outside the runtime" do
+      before do
+        allow(Iodine).to receive(:running?).and_return(false)
+      end
+
+      it "broadcasts the message" do
+        allow(Rage::Internal).to receive(:stream_name_for).with(streamable).and_return("test-stream-123")
+        allow(Rage::SSE).to receive(:__serialize).with(message).and_return(serialized_message)
+
+        expect(Rage::SSE::InternalBroadcast).not_to receive(:broadcast)
+        expect(mock_adapter).to receive(:publish).with(Rage::SSE::PUBSUB_BROADCASTER_ID, "test-stream-123", serialized_message)
+
+        described_class.broadcast(streamable, message)
+      end
+    end
+
+    context "without the adapter" do
+      before do
+        allow(Iodine).to receive(:running?).and_return(true)
+      end
+
+      let(:mock_adapter) { nil }
+
+      it "broadcasts the message" do
+        allow(Rage::Internal).to receive(:stream_name_for).with(streamable).and_return("test-stream-123")
+        allow(Rage::SSE).to receive(:__serialize).with(message).and_return(serialized_message)
+
+        expect(Rage::SSE::InternalBroadcast).to receive(:broadcast).with("test-stream-123", serialized_message, Iodine::PubSub::CLUSTER)
+
+        described_class.broadcast(streamable, message)
+      end
+    end
+  end
+
+  describe "InternalBroadcast.broadcast" do
+    subject { Rage::SSE::InternalBroadcast.broadcast(stream_name, data, engine) }
+
+    let(:stream_name) { "test-stream" }
+    let(:data) { double }
+    let(:engine) { double }
+
+    before do
+      Rage::SSE::Stream.__message_buffer.clear
+    end
+
+    it "publishes the message" do
+      expect(Iodine).to receive(:publish).with("sse:test-stream", data, engine)
+      subject
+    end
+
+    it "doesn't attempt to store a message" do
+      allow(Iodine).to receive(:publish)
+      subject
+      expect(Rage::SSE::Stream.__message_buffer).to be_empty
+    end
+
+    context "with an existing stream" do
+      before do
+        Rage::SSE::Stream.__message_buffer.clear
+        Rage::SSE.stream("test-stream")
+      end
+
+      after do
+        Rage::SSE::Stream.__message_buffer.clear
+      end
+
+      it "stores a message" do
+        allow(Iodine).to receive(:publish)
+        subject
+        expect(Rage::SSE::Stream.__message_buffer).to have_key("test-stream")
+      end
+    end
+  end
 end
