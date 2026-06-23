fb0834c0addabd916eb2283033fc043b515c467c
Mridankan Mandal <xerontitan90@gmail.com>
2026-06-24 01:07:19 +0530

    [Deferred] Ignore missing temp files during async cleanup (#334)
    * Fix deferred disk async cleanup crash
    
    Signed-off-by: Mridankan Mandal <xerontitan90@gmail.com>
    
    * Address deferred cleanup review feedback
    
    Signed-off-by: Mridankan Mandal <xerontitan90@gmail.com>
    
    ---------
    
    Signed-off-by: Mridankan Mandal <xerontitan90@gmail.com>

--- lib/rage/deferred/backends/disk.rb
+++ lib/rage/deferred/backends/disk.rb
@@ -241,8 +241,7 @@ class Rage::Deferred::Backends::Disk
 
     # delete the old storage ensuring the copied data has already been written to disk
     Iodine.run_after(@fsync_frequency) do
-      old_storage.close
-      File.unlink(old_storage.path)
+      cleanup_storage(old_storage)
     end
   end
 
@@ -267,8 +266,13 @@ class Rage::Deferred::Backends::Disk
     end
 
     Iodine.run_after(@fsync_frequency) do
-      storage.close
-      File.unlink(storage.path)
+      cleanup_storage(storage)
     end
+  end
+
+  def cleanup_storage(storage)
+    path = storage.path
+    storage.close
+    File.unlink(path) if File.exist?(path)
   end
 end
